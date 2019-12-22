69027afd995aef30687532d9a18d94703858a87d
Olga <olga.kohanova@gmail.com>
2019-12-22 12:58:34 +0300

    data binding, l_16

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,38 +1,51 @@
 package com.example.android.diceroller
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.databinding.DataBindingUtil
+import com.example.android.diceroller.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName:MyName = MyName("Olga")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        rollButton.setOnClickListener{
-            rollDice()
+//        findViewById<Button>(R.id.done_button).setOnClickListener{
+//            addNickName(it)
+//        }
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener{
+                        addNickName(it)
         }
     }
 
-    private fun rollDice() {
+    private fun addNickName(view: View) {
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        val randomInt:Int = Random().nextInt(6)+1
-        val diceImg:ImageView = findViewById(R.id.dice_img)
-        val drawableResource = when(randomInt){
-            1->R.drawable.dice_1
-            2->R.drawable.dice_2
-            3->R.drawable.dice_3
-            4->R.drawable.dice_4
-            5->R.drawable.dice_5
-            else->R.drawable.dice_6
+        binding.apply {
+            myName?.nickName = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
         }
-        diceImg.setImageResource(drawableResource)
-
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
+
+
 }
---
+++ app/src/main/java/com/example/android/diceroller/MyName.kt
@@ -0,0 +1,3 @@
+package com.example.android.diceroller
+
+data class MyName (var name: String = "", var nickName: String = "")
